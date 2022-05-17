9bc8a986b792dd4383826bfc11a0f40a372e165f
Andrei-Bond <andrei.bandarevich@mail.ru>
2022-05-17 13:41:59 +0300

    task pass

--- script.js
+++ script.js
@@ -52,12 +52,28 @@ if (num > 90 || num < 14) {
 } else {
   alert('не верно');
 }
-*/
-
 let num = prompt("Enter number", "");
 num = Number(num);
 if (!(num >= 14 && num <= 90)) {
   alert("верно");
 } else {
   alert('не верно');
+}
+*/
+let userPass;
+let userName = prompt("Ваше имя", "");
+if (userName === null) {
+  alert("Отменено");
+} else if(userName != "Админ") {
+  alert("Я вас не знаю");
+} else {
+  userPass = prompt("Введите пароль", "");
+}
+
+if (userPass === null) {
+  alert("Отменено");
+} else if( userPass != "Я главный") {
+  alert("Неверный пароль");
+} else {
+  alert("Здравствуйте!");
 }
