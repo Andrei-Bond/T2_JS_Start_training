defcf8569d1836301cec25e1a7aa5e92fbeb624f
Andrei-Bond <andrei.bandarevich@mail.ru>
2022-05-17 13:18:57 +0300

    !

--- script.js
+++ script.js
@@ -43,11 +43,20 @@ let message = (login == 'Сотрудник') ? 'Привет' :
   (login == '') ? 'Нет логина' : 
   '';
 alert(message);
-*/
+
 
 let num = prompt('enter number','');
 num = Number(num);
 if (num > 90 || num < 14) {
+  alert("верно");
+} else {
+  alert('не верно');
+}
+*/
+
+let num = prompt("Enter number", "");
+num = Number(num);
+if (!(num >= 14 && num <= 90)) {
   alert("верно");
 } else {
   alert('не верно');
