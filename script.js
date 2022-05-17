1f32d7bcd5c81829ca2395ecc873f5450d7f3364
1 <1>
2022-05-17 10:47:19 +0300

    log. operators

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 "use strict"
-let num = prompt('enter number', 5);
+/*let num = prompt('enter number', 5);
 
 function fac(num2){
   
@@ -12,3 +12,43 @@ function fac(num2){
 console.log(fac(num));
 
 alert(fac(num));
+
+let ofCallJS = prompt('Какое официальноеназвание JS', ' ');
+if (ofCallJS == "ECMAScript") {
+  alert('Верно!');
+}  else {
+  alert('Не знаете? ECMAScript');
+}
+
+
+let num = prompt('Enter number', '');
+if (num > 0) {
+  alert(1);
+} else if (num < 0) {
+  alert(-1);
+} else if (num == 0) {
+  alert(0);
+} else {
+  alert('Wrong entering');
+}
+
+let a = 1 , b = 1;
+let result = (a + b < 4) ? 'Мало' :'Много';
+console.log(result);
+
+
+let login = prompt('');
+let message = (login == 'Сотрудник') ? 'Привет' :
+  (login == 'Директор') ? 'Здравствуйте':
+  (login == '') ? 'Нет логина' : 
+  '';
+alert(message);
+*/
+
+let num = prompt('enter number','');
+num = Number(num);
+if (num > 90 || num < 14) {
+  alert("верно");
+} else {
+  alert('не верно');
+}
