91bbe4669c88d9c9b44a31cd50d9b5dbf13c5f4d
andrei.bandarevich@mail.ru <andrei.bandarevich@mail.ru>
2022-05-20 22:03:37 +0300

    "switch"

--- script.js
+++ script.js
@@ -167,7 +167,7 @@ for (let num = prompt("Введите число больше ста", ""); num 
   if( num === null) break;
 }
 
-*/
+
 let i = 2;
 first:
 for (let n = prompt("Enter number",""); i <= n; i++ ) {
@@ -179,4 +179,31 @@ for (let n = prompt("Enter number",""); i <= n; i++ ) {
   alert(i);
     
   
+}
+
+let browser = "Opera";
+if (browser === "Edge") {
+  alert( "You've got the Edge!" );
+} else if (browser === 'Chrome' || 'Firefox' ||'Safari' || 'Opera') {
+  alert( 'Okay we support these browsers too' );
+} else {
+  alert( 'We hope that this page looks ok!' );
+}
+
+*/
+
+const number = +prompt('Введите число между 0 и 3', '');
+switch(number){
+  case 0:
+    alert('Вы ввели число 0');
+    break;
+    
+  case 1:
+  alert('Вы ввели число 1');
+  break;
+  
+  case 2:
+  case 3:
+   alert('Вы ввели число 2, а может и 3');
+   break;
 }
