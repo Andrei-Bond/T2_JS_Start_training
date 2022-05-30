77b16ce8329472bc39ad2ded0f5c76dd1237084c
Andrei-Bond <andrei.bandarevich@mail.ru>
2022-05-30 12:18:08 +0300

    edit tests - add cycle

--- test.js
+++ test.js
@@ -3,14 +3,26 @@
 
 describe("pow", function() {
 
-    function makeTest(x) {
+    function makeTest3(x) {
         let expected = x * x * x;
         it (`${x} в степени 3 будет ${expected}`, function() {
             assert.equal(pow(x, 3), expected);
     });
     }
 
-    for (let x = 1; x <= 5; x++) {
-        makeTest(x);
+    for (let x = 1; x <= 3; x++) {
+        makeTest3(x);
+    } 
+
+
+    function makeTest7(y) {
+        let expected = y * y * y * y * y * y * y;
+        it (`${y} в степени 7 будет ${expected}`, function() {
+            assert.equal(pow(y, 7), expected);
+    });
+    }
+
+    for (let y = 1; y <= 3; y++) {
+        makeTest7(y);
     } 
 }) ;
