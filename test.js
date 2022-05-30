070bd0aa0a83899df9561308c12ec0aadbe3aff8
Andrei-Bond <andrei.bandarevich@mail.ru>
2022-05-30 12:25:14 +0300

    edit tests: describe*2

--- test.js
+++ test.js
@@ -3,7 +3,8 @@
 
 describe("pow", function() {
 
-    function makeTest3(x) {
+    describe("Возводит x в степень 3", function() {
+        function makeTest3(x) {
         let expected = x * x * x;
         it (`${x} в степени 3 будет ${expected}`, function() {
             assert.equal(pow(x, 3), expected);
@@ -12,10 +13,11 @@ describe("pow", function() {
 
     for (let x = 1; x <= 3; x++) {
         makeTest3(x);
-    } 
+    } } );
 
 
-    function makeTest7(y) {
+    describe("Возводит y в степень 7", function() {
+        function makeTest7(y) {
         let expected = y * y * y * y * y * y * y;
         it (`${y} в степени 7 будет ${expected}`, function() {
             assert.equal(pow(y, 7), expected);
@@ -24,5 +26,5 @@ describe("pow", function() {
 
     for (let y = 1; y <= 3; y++) {
         makeTest7(y);
-    } 
+    } } );
 }) ;
