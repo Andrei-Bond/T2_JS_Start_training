bef0e2315272c8af4abb923104410c2d80a59e01
Andrei-Bond <andrei.bandarevich@mail.ru>
2022-06-17 15:29:52 +0300

    string p.2

--- script.js
+++ script.js
@@ -690,4 +690,24 @@ alert(checkSpam('buy ViAgRA now'));
 alert(checkSpam('free xxxxx'));
 alert(checkSpam("innocent rabbit"));
 
+
+
+
+function truncate(str, maxlength) {
+  if (str.length <= maxlength ) {
+    return str;
+  } else {
+    return `${str.slice(0, maxlength-1)}…`;
+  }
+}
+
+alert(truncate("Вот, что мне хотелось бы сказать на эту тему:", 20));
+
+alert(truncate("Всем привет!", 20));
+
+
 */
+
+function extractCurrencyValue(str) {
+  return +str.slice(0);
+}
