4d14e0d837938a3fa15a60a8bdbc93dbbbf090f5
andrei.bandarevich@mail.ru <andrei.bandarevich@mail.ru>
2022-06-30 19:42:10 +0300

    Object.keys(), values(), entries()

--- script.js
+++ script.js
@@ -1195,7 +1195,7 @@ console.log(keys);
 // Ошибка: keys.push -- это не функция
 keys.push("more");
 
-*/
+
 
 let messages = [
   { text: "Hello", from: "John" },
@@ -1232,3 +1232,43 @@ function addTime(mess, time) {
 
 addTime(messages[1], '16:53 12.07.22');
 alert(timeReadMessage.get(messages[1]));
+
+
+
+
+
+let salaries = {
+  "John": 100,
+  "Pete": 300,
+  "Mary": 250
+};
+
+function sumSalaries(obj) {
+  let sum = 0;
+  for (let values of Object.values(obj)) {
+    sum += values;
+  }
+  return sum;
+}
+
+
+
+alert(sumSalaries(salaries)); // 650
+
+*/
+
+let user = {
+  name: 'John',
+  age: 30
+};
+
+function count(obj) {
+  let sum = 0;
+  for(let key of Object.keys(obj)) {
+    sum++;
+  }
+  return sum;
+}
+
+
+alert(count(user)); // 2
